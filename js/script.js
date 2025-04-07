ad34d94e2a931e6d7fc119e63e09e579ce28fd84
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-04-07 21:45:01 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Benjamin Abebe
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+
+function myButtonClicked() {
+ // input
+ const TEMP_FAHRENHEIT = parseFloat(document.getElementById("fahrenheit-input").value)
+
+
+ // process
+ const TEMP_CELSIUS = Math.round((TEMP_FAHRENHEIT - 32) * 5/9).toFixed(2)
+
+
+ // output
+ document.getElementById('answer').innerHTML = "If the Temperature in Fahrenheit is " + TEMP_FAHRENHEIT + ", Then the temperature in calsius will be: " + TEMP_CELSIUS + "."
+}
